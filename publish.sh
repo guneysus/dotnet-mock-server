b80be3b73ab421ed1a8980c626a70e1d14664d24
Ahmed Şeref Güneysu <serefguneysu@gmail.com>
2020-05-23 02:43:06 +0300

    os windows

--- publish.sh
+++ publish.sh
@@ -3,8 +3,9 @@
 dotnet publish src/dotnet-mock-server/dotnet-mock-server.csproj \
 	--configuration Release \
 	--output build \
+	--no-build \
 	-p:PublishReadyToRun=false \
 	-p:PublishSingleFile=true \
-	-p:PublishTrimmed=false \
+	-p:PublishTrimmed=true \
 	--self-contained true \
 	--runtime win-x64
