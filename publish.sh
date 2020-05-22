96211376586a8d337e520f5dde12727ef835a1fb
Ahmed Şeref Güneysu <serefguneysu@gmail.com>
2020-05-23 02:36:21 +0300

    publish.sh fix

--- publish.sh
+++ publish.sh
@@ -3,10 +3,9 @@
 dotnet publish src/dotnet-mock-server/dotnet-mock-server.csproj \
 	--configuration Release \
 	--output build \
+	--no-build \
 	-p:PublishReadyToRun=false \
 	-p:PublishSingleFile=true \
 	-p:PublishTrimmed=true \
 	--self-contained true \
-	--runtime win-x64 && \
-	rm build/*.pdb && 
-	mv build/dotnet-mock-server.exe build/nmock.exe	
+	--runtime win-x64
