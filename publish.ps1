7a035b6e3ecc3f21919fd8f62a4c17ea640bbc60
Ahmed Şeref Güneysu <serefguneysu@gmail.com>
2020-05-22 11:31:57 +0300

    publish script for single exe

---
+++ publish.ps1
@@ -0,0 +1,10 @@
+dotnet publish .\src\dotnet-mock-server\dotnet-mock-server.csproj `
+	--configuration Release `
+	--output build `
+	-p:PublishReadyToRun=true `
+	-p:PublishSingleFile=true `
+	-p:PublishTrimmed=true `
+	--self-contained true `
+	--runtime win-x64 
+	
+	# -p:PublishReadyToRunShowWarnings=true
